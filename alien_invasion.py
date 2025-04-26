583ae7e91097c5721f7c062aa3a72da017fbe250
gengwg <genwg@users.noreply.github.com>
2025-04-26 09:40:44 -0700

    store bullets in a group

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,10 +21,11 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        
+
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """start the main loop for the game"""
@@ -32,6 +33,7 @@ class AlienInvasion:
 
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
