c2cf4a718dd52bf82e26d0a2a03f51a715e2c6f9
gengwg <genwg@users.noreply.github.com>
2025-04-26 09:57:21 -0700

    delete old bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,13 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            # get rid of bullets that reahc top of the screen.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            # print(len(self.bullets))
+
             self._update_screen()
 
     def _check_events(self):
