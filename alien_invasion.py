35168b5fb41fd66bdb3f59168dc69f9636f7d404
gengwg <gengwg@users.noreply.github.com>
2025-05-01 16:10:46 -0700

    identify the parts that should always run and that should only when the game is active

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,9 +42,11 @@ class AlienInvasion:
         while True:
 
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update() 
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
 
     def _update_bullets(self):
