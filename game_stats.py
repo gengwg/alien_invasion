aab24354cd4b5a846ed6eda8ae14229709e9da1c
gengwg <gengwg@users.noreply.github.com>
2025-05-01 16:04:39 -0700

    game over

--- game_stats.py
+++ game_stats.py
@@ -6,8 +6,8 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-        # Start Alien Invasion in an inactive state.
-        self.game_active = False
+        # Start Alien Invasion in an active state.
+        self.game_active = True
 
         # High score should never be reset.
         self.high_score = 0
